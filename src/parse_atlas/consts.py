0d224663b7a909fd7cf569f849f116de4924b131
Zhavi221 <netalevy2059@gmail.com>
2025-12-31 17:25:11 +0200

    refactor start

--- src/parse_atlas/consts.py
+++ src/parse_atlas/consts.py
@@ -1,1 +1,9 @@
 CMS_RECID_FILEPAGE_URL = "https://opendata.cern.ch/record/{0}/filepage/1?group=1"
+
+# CMS field indicators for matching branches
+CMS_FIELD_INDICATORS = {
+    "pt": "fPt",
+    "eta": "fEta",
+    "phi": "fPhi",
+    "mass": "fM"
+}
